a6dc8fcd5dd5eaa0c758f09e834c367e2a93bd84
L.Jay <bozhancom@163.com>
2024-07-30 00:42:07 +0800

    feat: add useFrequencyEffect

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,16 +1,8 @@
 import useWithResolvers from "./useWithResolvers";
 import useOnChangeValue from "./useOnChangeValue";
-<<<<<<< HEAD
-import useF
-
-export {
-  useWithResolvers,
-  useOnChangeValue,
-=======
 import useFrequencyEffect from "./useFrequencyEffect";
 export {
   useWithResolvers,
   useOnChangeValue,
   useFrequencyEffect,
->>>>>>> useOnChangeValue
 }
